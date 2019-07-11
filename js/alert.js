296ad7c8fcbda10d5cd1f939e874e913f846486f
tiennesdm <shubahm.mehta@gmail.com>
2019-07-12 00:32:34 +0530

    method define

--- js/alert.js
+++ js/alert.js
@@ -7,6 +7,20 @@ new Vue({
         turns: []
     },
     methods: {
+        startGame: function() {
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.turns = [];
+
+        },
+        attack: function() {},
+        specialAttack: function() {},
+        heal: function() {},
+        giveUp: function() {},
+        monsterAttacks: function() {},
+        calculateDamage: function() {},
+        checkWin: function() {}
 
 
     },
