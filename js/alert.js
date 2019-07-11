bd0d87fbac557f88872a79023e951c360d9e9c48
tiennesdm <shubahm.mehta@gmail.com>
2019-07-12 00:12:35 +0530

    vue js style and data print

---
+++ js/alert.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: "#app",
+    data: {
+        playerHealth: 100,
+        monsterHealth: 100,
+        gameIsRunning: false,
+        turns: []
+    },
+    methods: {
+
+
+    },
+});
